3e8c3f8f3357ac808f11aed00a690c75d5dbc305
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-07-28 09:52:56 -0400

    Update BugController.js

--- controllers/BugController.js
+++ controllers/BugController.js
@@ -97,8 +97,7 @@ export const deleteImage = async (req, res) => {
     const { projectId, bugId, imageId } = req.params;
     if (!mongoose.Types.ObjectId.isValid(bugId)) return res.status(404).send(`No bug with id: ${bugId}`);
         await ProjectModel.findOneAndUpdate(
-            { _id: projectId },
-            { 'bugs': { _id: bugId }  },
+            { _id: projectId, 'bugs._id': bugId },
             { $pull: { 'images': { _id: imageId }}},
             { multi: true }
         )
