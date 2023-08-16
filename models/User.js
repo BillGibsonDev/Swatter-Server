47b109565ac1cf1c06683dd324db6a211a99c250
Bill Gibson <71801512+BillGibsonDev@users.noreply.github.com>
2023-08-15 21:24:56 -0400

    init

--- models/User.js
+++ models/User.js
@@ -1,5 +1,65 @@
 import mongoose from 'mongoose';
 
+// children
+const ImagesSchema = new mongoose.Schema({
+    image: String,
+    caption: String,
+})
+
+const BugCommentSchema = new mongoose.Schema({
+    comment: String,
+    date: String,
+    author: String,
+    avatar: String,
+})
+
+const BugSchema = new mongoose.Schema({
+    title:  String,
+    description: String,
+    date: String,
+    lastUpdate: String,
+    bugKey: String,
+    images: [{ type: ImagesSchema, ref: 'bugImage'}],
+    status: String,
+    author: String,
+    priority: String,
+    tag: String,
+    sprint: String,
+    comments: [{type: BugCommentSchema, ref: 'bugComment'}]
+})
+
+const CommentSchema = new mongoose.Schema({
+    comment: String,
+    date: String,
+    author: String,
+})
+
+const SprintSchema = new mongoose.Schema({
+    title: String,
+    goal: String,
+    color: String,
+    endDate: String,
+    updated: String,
+    status: String,
+})
+
+// parent
+const ProjectSchema = new mongoose.Schema({
+    projectTitle: String,
+    startDate: String,   
+    lastUpdate: String,
+    projectLead: String,
+    projectImage: String,
+    projectLink: String,
+    projectType: String,
+    description: String,
+    projectKey: String,
+    repository: String,
+    bugs: [{ type: BugSchema, ref: "bugs" }],
+    comments: [{ type: CommentSchema, ref: "comments" }],
+    sprints: [{type: SprintSchema, ref: "sprints"}]
+})
+
 // parent
 const UserSchema = new mongoose.Schema({
     username: {
@@ -7,19 +67,28 @@ const UserSchema = new mongoose.Schema({
         allowNull: false,
         unique: true,
     },
-    token: {
-        type: String,
-    },
     password: {
         type: String,
         allowNull: false,
     },
-    role: {
+    email: {
         type: String,
+        allowNull: false,
+        unique: true,
     },
-    lastLogin: {
-        type: String,
-    }
+    token: String,
+    role: String,
+    lastLogin: String,
+    team: [{
+        username: {
+            type: String,
+            unique: true,
+        }
+    }],
+    projects: [{
+        type: ProjectSchema,
+        ref: 'projects'
+    }]
 })
 
 export const UserModel = mongoose.model("User", UserSchema)
